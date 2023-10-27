2d203641defaf6dbc93b502ba1ed2045cbc97e50
AnzhelikaGerasimova <ababayan2041@gmail.com>
2023-10-27 20:58:00 +0300

    Extra tasks

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+    static double square(double side) {
+        return Math.pow(side, 2);
+    }
+    static double circle(double radius) {
+        return Math.pow(radius, 2)*Math.PI;
+    }
+    static double triangle(double side1, double side2, double side3) {
+        double p = (side1+side2+side3)/2;
+        return Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(square(11));
+        System.out.println(circle(5));
+        System.out.println(triangle(10, 18, 25));
+    }
 }
