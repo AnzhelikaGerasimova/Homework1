13ab55068c89e201801252eb48df3ecd6ad0b321
AnzhelikaGerasimova <ababayan2041@gmail.com>
2023-10-28 15:53:18 +0300

    Extra tasks included

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,13 +2,16 @@ public class Hello {
     static double square(double side) {
         return Math.pow(side, 2);
     }
+
     static double circle(double radius) {
-        return Math.pow(radius, 2)*Math.PI;
+        return Math.pow(radius, 2) * Math.PI;
     }
+
     static double triangle(double side1, double side2, double side3) {
-        double p = (side1+side2+side3)/2;
-        return Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+        double p = (side1 + side2 + side3) / 2;
+        return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
     }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(square(11));
